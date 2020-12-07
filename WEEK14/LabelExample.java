8c500bb82cbc328b6b6b0edfb9a353455613a2ca
Chardy C. Fernando <ccfernando@crc.pshs.edu.ph>
2020-12-07 09:58:37 +0800

    Create LabelExample.java

---
+++ WEEK14/LabelExample.java
@@ -0,0 +1,15 @@
+ class LabelExample {
+     public static void main(String args[]) {
+         Frame MyFrame = new Frame("Example");
+         Label luser, lpass;
+         luser = new Label("Username:");
+         luser.setBounds(50, 100, 100, 30);
+         lpass = new Label("Password:");
+         lpass.setBounds(50, 150, 100, 30);
+         MyFrame add(luser);
+         MyFrame.add(lpass);
+         MyFrame.setSize(400, 400);
+         MyFrame.setLayout(null);
+         MyFrame.setVisible(true);
+     }
+ }
